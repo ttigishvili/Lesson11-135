9b49d09dccda5f61062f95ad92d658c02e0da5e9
ttigishvili <ttigishvili@gmail.com>
2022-07-16 18:07:03 +0400

    123

---
+++ scriptsandro.js
@@ -0,0 +1,40 @@
+
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'];
+// console.log(array [0], array [1], array [array.length-2], array [array.length-1]);
+
+// let array = [32, 10, 'hello', null, 'hello2', 50];
+// for (let item of array){
+//     if (item % 5 ==0){
+//         console.log(item);
+//     }
+// }
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let item of numbers){
+//     if (item > 5){
+//         console.log(item);
+//     }
+// }
+
+// let  user = {
+//     firstname: 'giorgi',
+//     lastname: 'smith',
+//      age: 25,
+//    studentstatus: 'active',
+//    };
+//    console.log(user.studentstatus);
+   
+   let user = {
+	name: 'giorgi',
+	age:  20,
+	studentstatus: 'active',
+};
+if (user.age > 18, user.studentstatus = 'active'){
+    console.log('hello');
+}if (user.name = 'giorgi'){
+    console.log('hello giorgi');
+}if (user.studentstatus = 'active' || user.age < 25){
+    console.log('hello world');
+}else {
+    console.log('error');
+}
